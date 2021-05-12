20150c7c33225245a68720ac22628bce636670f7
Pavel Shift <shift.pavel@mail.ru>
2021-05-12 19:39:05 +0400

    + sort + podschet

--- numpy_test.py
+++ numpy_test.py
@@ -1,3 +1,10 @@
 import numpy as np
-array = np.random.rand(3, 3)
+
+array = np.rint(10 * np.random.rand(3, 3))
 print(array)
+
+array_sort_x = np.sort(array, axis = 1)
+print( array_sort_x)
+
+array_podschet_x = np.count_nonzero(array, axis= 1)
+print(array_podschet_x)
